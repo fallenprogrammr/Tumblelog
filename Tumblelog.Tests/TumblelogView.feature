e0b20e8fc2e18c765e026dd73cece555120ea667
Amit Patankar <amit.patankar@gmail.com>
2014-05-11 00:24:10 -0500

    Initial commit

---
+++ Tumblelog.Tests/TumblelogView.feature
@@ -0,0 +1,10 @@
+﻿Feature: TumblelogView
+	In order to launch my blog
+    I have to make my tumblelog site viewable
+
+@homepage
+Scenario: Browse to home page
+	Given I have to view the home page
+	When I browse to the home page
+	Then the page should have the title of 'My Tumblelog'
+    And the heading on the page should be 'My Tumblelog'
---
+++ Tumblelog.Tests/TumblelogViewSteps.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.PhantomJS;
+using TechTalk.SpecFlow;
+
+namespace Tumblelog.Tests {
+    [Binding]
+    public class TumblelogViewSteps {
+        private IWebDriver driver;
+
+        [Given(@"I have to view the home page")]
+        public void GivenIHaveToViewTheHomePage() {
+            driver = new PhantomJSDriver();
+            driver.Manage().Timeouts().ImplicitlyWait(new TimeSpan(0, 0, 10));
+        }
+
+        [When(@"I browse to the home page")]
+        public void WhenIBrowseToTheHomePage() {
+            driver.Navigate().GoToUrl(ConfigurationManager.AppSettings["home"]);
+        }
+
+        [Then(@"the page should have the title of '(.*)'")]
+        public void ThenThePageShouldHaveTheTitleOf(string p0) {
+            Assert.AreEqual(p0, driver.Title);
+        }
+
+        [Then(@"the heading on the page should be '(.*)'")]
+        public void ThenTheHeadingOnThePageShouldBe(string p0) {
+            Assert.AreEqual(p0, driver.FindElement(By.Id("PageHeading")).Text);
+        }
+    }
+}
